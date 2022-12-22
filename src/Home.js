c4cba575223bf890e2c6cbeb3242ee12835b3ff8
Eric Thande <ericthande@Erics-MacBook-Pro.local>
2022-12-22 14:10:54 +0200

    formated the code with prettier

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Link} from 'react-router-dom'
-import Navbar from './Navbar';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function Home() {
-  const [countries,setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
   const [chunks, setChunks] = useState([]);
   const [currentChunk, setCurrentChunk] = useState(() => 0);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('https://restcountries.com/v3.1/all');
+      const response = await fetch("https://restcountries.com/v3.1/all");
       const data = await response.json();
       setCountries(() => data);
 
@@ -18,41 +18,54 @@ function Home() {
       for (let i = 0; i < data.length; i += chunkSize) {
         chunks.push(data.slice(i, i + chunkSize));
       }
-      setChunks(()=> chunks);
+      setChunks(() => chunks);
     }
 
     fetchData();
-  }, [countries,chunks]);
+  }, [countries, chunks]);
 
   const chunk = chunks[currentChunk] || [];
   return (
-<div className='container'>
-  <span>
-    <Navbar />
-  </span>
-    <ul>
-    {chunk.map((country) => (
-    <li className='item' key={country.cca3}>
-    <h3>
-    <Link className='name' to={`/countries/${country.cca3}`}>
-+      {country.name.common}
-     </Link>        </h3>
+    <div className="container">
+      <span>
+        <Navbar />
+      </span>
+      <ul>
+        {chunk.map((country) => (
+          <li className="item" key={country.cca3}>
+            <h3>
+              <Link className="name" to={`/countries/${country.cca3}`}>
+                + {country.name.common}
+              </Link>{" "}
+            </h3>
             <p>
               Region: {country.region}
               <br />
-              Languages: {Object.values(country.languages).map((language) => language).join(', ')}              <br />
-               <img src={country.flags.svg} className="flag" alt={`Flag of ${country.name.common}`} />
+              Languages:{" "}
+              {Object.values(country.languages)
+                .map((language) => language)
+                .join(", ")}{" "}
+              <br />
+              <img
+                src={country.flags.svg}
+                className="flag"
+                alt={`Flag of ${country.name.common}`}
+              />
             </p>
           </li>
         ))}
       </ul>
       {currentChunk > 0 && (
-        <button className='button' onClick={() => setCurrentChunk((prevChunk) => prevChunk - 1)}>
+        <button
+          className="button"
+          onClick={() => setCurrentChunk((prevChunk) => prevChunk - 1)}>
           Prev
         </button>
       )}
       {currentChunk < chunks.length - 1 && (
-        <button className='button' onClick={() => setCurrentChunk((prevChunk) => prevChunk + 1)}>
+        <button
+          className="button"
+          onClick={() => setCurrentChunk((prevChunk) => prevChunk + 1)}>
           Next
         </button>
       )}
