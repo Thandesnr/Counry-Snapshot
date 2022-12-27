a6c7a1157141376e5448cf5cda53db89f93bd285
Eric Thande <ericthande@Erics-MacBook-Pro.local>
2022-12-27 13:35:47 +0200

    sorted the countries in alphabetical order

--- src/Home.js
+++ src/Home.js
@@ -7,42 +7,45 @@ function Home() {
   const [chunks, setChunks] = useState([]);
   const [currentChunk, setCurrentChunk] = useState(() => 0);
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("https://restcountries.com/v3.1/all");
-      const data = await response.json();
-      setCountries(() => data);
+useEffect(() => {
+  async function fetchData() {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    let data = await response.json();
 
-      const chunkSize = 5;
-      const chunks = [];
-      for (let i = 0; i < data.length; i += chunkSize) {
-        chunks.push(data.slice(i, i + chunkSize));
-      }
-      setChunks(() => chunks);
+    data.sort((a, b) => String(a.name.common).localeCompare(String(b.name.common)));
+    setCountries(data);
+
+    const chunkSize = 5;
+    const chunks = [];
+    for (let i = 0; i < data.length; i += chunkSize) {
+      chunks.push(data.slice(i, i + chunkSize));
     }
-
-    fetchData();
-  }, [countries, chunks]);
+    setChunks(chunks);
+  }
+  fetchData();
+}, []);
 
   const chunk = chunks[currentChunk] || [];
+
   return (
     <div className="container">
       <span>
         <Navbar />
       </span>
+      <br />
       <ul>
         {chunk.map((country) => (
           <li className="item" key={country.cca3}>
             <h3>
               <Link className="name" to={`/countries/${country.cca3}`}>
-                + {country.name.common}
+                 {country.name.common}
               </Link>{" "}
             </h3>
             <p>
               Region: {country.region}
               <br />
               Languages:{" "}
-              {Object.values(country.languages)
+              {Object.values(country.languages || {})
                 .map((language) => language)
                 .join(", ")}{" "}
               <br />
@@ -58,7 +61,11 @@ function Home() {
       {currentChunk > 0 && (
         <button
           className="button"
-          onClick={() => setCurrentChunk((prevChunk) => prevChunk - 1)}>
+          onClick={() => {setCurrentChunk((prevChunk) => {
+            console.log(prevChunk)
+            return prevChunk - 1;});
+
+          }}>
           Prev
         </button>
       )}
